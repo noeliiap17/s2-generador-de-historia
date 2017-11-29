4346150b6fab11d5760055fdb2ee5919f027942d
Noeliiap17 <n.parra2003@gmail.com>
2017-11-29 09:37:56 +0100

    ejercicio resuelto

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var usuario = prompt('¿A que plataforma perteneces?');
+var funcionalidad = prompt('Dime tu funcionalidad');
+var razón = prompt('Dime tus razones');
+alert('Yo como un cliente de ' + usuario + ' \nnecesito ' + funcionalidad + ' \ncon la finalidad de ' + razón );
